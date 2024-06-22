9885c2705c28671d43e725301edf493fcb31ddce
Ivan Ryzhkovich <yatchic132@gmail.com>
2024-06-23 01:22:35 +0300

    измененный проект

--- src/ru/gb/vending_machine/Main.java
+++ src/ru/gb/vending_machine/Main.java
@@ -1,11 +1,14 @@
 package ru.gb.vending_machine;
 
 import ru.gb.vending_machine.products.Bottle;
+import ru.gb.vending_machine.products.HotDrink;
 import ru.gb.vending_machine.products.Product;
+import ru.gb.vending_machine.vending.HotDrinkVendingMachine;
 import ru.gb.vending_machine.vending.VendingMachine;
 
 public class Main {
     public static void main(String[] args) {
+
         VendingMachine vendingMachine = new VendingMachine(123);
 
         Product product1 = new Bottle("coca-cola", 150, 1.5);
@@ -15,5 +18,19 @@ public class Main {
         vendingMachine.addProduct(product2);
 
         System.out.println(vendingMachine.getProductsInfo());
+
+
+        HotDrinkVendingMachine hotDrinkVendingMachine = new HotDrinkVendingMachine(456);
+
+        HotDrink hotDrink1 = new HotDrink("coffee", 200, 80);
+        HotDrink hotDrink2 = new HotDrink("tea", 150, 75);
+
+        hotDrinkVendingMachine.addProduct(hotDrink1);
+        hotDrinkVendingMachine.addProduct(hotDrink2);
+
+
+
+
+        System.out.println(hotDrinkVendingMachine.getProductsInfo());
     }
 }
